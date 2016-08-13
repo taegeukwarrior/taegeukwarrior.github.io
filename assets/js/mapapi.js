49703d8752c12bc41b388f89ab133c6d148658f7
Afif Hendrawan <afif.hendrawan@gmail.com>
2016-08-13 17:05:34 +0700

    map update

--- assets/js/mapapi.js
+++ assets/js/mapapi.js
@@ -1,7 +1,7 @@
 function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: {lat: -6.748732, lng: 111.027924},
-    zoom: 8
-  });
+var mapDiv = document.getElementById('map');
+var map = new google.maps.Map(mapDiv, {
+  center: {lat: -6.748732, lng: 111.027924},
+  zoom: 8
+});
 }
